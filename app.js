2d5713c242e9ea4d466981d0e81bc45badf5e08a
squirrel <michaelwalker@MICHAELs-MBP.attlocal.net>
2021-11-20 22:08:42 -0800

    clean up andadd Clear btn fn

--- app.js
+++ app.js
@@ -11,46 +11,49 @@ function handleClick(e) {
 
   const todoList = document.querySelector('ul');
 
-  if (value != '') {
+  console.log({ name,value,submitter },submitter.className,submitter.id);
 
-    switch (submitter.id) {
-      case 'addBtn':
-        addTodo(value);
-        input.value = "";
-        focusInput();
-        console.log({ name,value,submitter },submitter);
-        break;
-      case 'delBtn':
-        break;
-      case 'clearBtn':
-        break;
-      default:
-        input.value = "";
-        focusInput();
-    }
-
-
+  if (value != '' && submitter.id === "addBtn") {
+    addTodo(value);
   }
+
+  if (submitter.id === 'clearBtn') {
+    console.log('Cleared all elements');
+    todoList.innerHTML = '';
+  }
+  // case 'delBtn':
+  //   console.log({ name,value,submitter },submitter);
+
+  //   alert('trying')
+  //   deleteTodo()
+  //   break;
+  // case 'clearBtn':
+  focusInput();
+  input.value = "";
 }
+
 
 function addTodo(value) {
   const ul = document.querySelector('ul');
   const li = document.createElement('li');
   li.innerHTML = `
-    <li class="item">
+    <li class="todo">
+    <div> ${value} </div>
     <div>
-    ${value}
+      <button class="delBtn" type="submit"> Delete </button>
     </div>
-    <button class="delBtn" type="submit"> Delete </button>
     </li>
-    `;
-
-
+  `;
   ul.appendChild(li);
+
   console.log(`===> ADDED newItem ${value}`);
+}
+
+function deleteTodo() {
+  const value = 'something';
+  alert(`DELETED Item ${value}`)
 }
 
 function focusInput() {
   document.querySelector('input').focus();
-  console.log('Re-focused')
 }
